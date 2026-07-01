c5c9eca7797fd069999090415310f00feaa421c9
Keitooo <kiteczek.biznes@gmail.com>
2026-07-01 22:43:48 +0200

    custom items yay

--- src/main/java/com/keitooo/testingmod/testingmod.java
+++ src/main/java/com/keitooo/testingmod/testingmod.java
@@ -1,5 +1,6 @@
 package com.keitooo.testingmod;
 
+import com.keitooo.testingmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -48,6 +49,8 @@ public class testingmod {
 
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -61,7 +64,9 @@ public class testingmod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SUNSTONE); // added an item to the ingredients tab
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
