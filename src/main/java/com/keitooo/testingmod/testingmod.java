46dd21db830e692f847cd808009fd4dbf11e2d7a
Keitooo <kiteczek.biznes@gmail.com>
2026-07-02 01:20:08 +0200

    Recipes and loot tables

--- src/main/java/com/keitooo/testingmod/testingmod.java
+++ src/main/java/com/keitooo/testingmod/testingmod.java
@@ -1,24 +1,13 @@
 package com.keitooo.testingmod;
 
 import com.keitooo.testingmod.block.ModBlocks;
+import com.keitooo.testingmod.item.ModCreativeModeTabs;
 import com.keitooo.testingmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -28,10 +17,6 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(testingmod.MODID)
@@ -49,6 +34,8 @@ public class testingmod {
         modEventBus.addListener(this::commonSetup);
 
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
@@ -73,6 +60,7 @@ public class testingmod {
         if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
             event.accept(ModBlocks.ROSE_GOLD_BLOCK);
             event.accept(ModBlocks.SUNSTONE_ORE);
+            event.accept(ModBlocks.DEEPSLATE_SUNSTONE_ORE);
         }
     }
 
